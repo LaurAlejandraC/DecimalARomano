cf13f264e4699be9a2fe0d87fad21d9e5ae3699b
LaurAlejandraC <lauralejandracg@gmail.com>
2015-07-15 16:41:17 -0500

    Se añade la funcionalidad para los números romanos con barra, exceptuando el 4 y 9

--- decimalARomano.java
+++ decimalARomano.java
@@ -14,7 +14,7 @@ public class decimalARomano{
 	public int[] digitos(int numeroDecimal){
 		int[] resultadoDigitos;
 		int nDigitos = numeroDigitos(numeroDecimal);
-		resultadoDigitos = new int[nDigitos-1];
+		resultadoDigitos = new int[nDigitos];
 		for(int i = 0; i < nDigitos; i++){
 			resultadoDigitos[nDigitos - 1] = numeroDecimal%10;
 			numeroDecimal /= 10; 
@@ -25,19 +25,54 @@ public class decimalARomano{
 	public String numeroDecimalARomano(int numeroDecimal){
 		int nDigitos = numeroDigitos(numeroDecimal);
 		do{
+			int[] digitos = digitos(numeroDecimal);
 			if ( numeroDecimal >= 3999)
-				numeroDecimal = numeroRomanoBarra(numeroDecimal);
+				numeroDecimal = numeroRomanoBarra(numeroDecimal, digitos);
 			else
-				numeroDecimal = numeroRomano(numeroDecimal);
-			return resultado;
+				numeroDecimal = numeroRomano(numeroDecimal, digitos);
 		}while (numeroDecimal != 0);
+		return resultado;
 	}
 
-	public int numeroRomanoBarra(int numeroDecimal){
+	public int numeroRomanoBarra(int numeroDecimal, int[] digitos){
+		int nDigitos = numeroDigitos(numeroDecimal);
+		nDigitos -= 4;
+		if( digitos[0] >= 5 && digitos[0] != 9){
+			switch(nDigitos){
+				case 0: resultado += 'Ṽ';
+						numeroDecimal -= 5000;
+						break;
+				case 1: resultado += 'Ḹ';
+						numeroDecimal -= 50000;
+						break;
+				case 2: resultado += 'Ď';
+						numeroDecimal -= 500000;
+						break;
+			}
+		}else if(digitos[0] != 4 && digitos[0] != 9){
+			switch(nDigitos){
+				case 0: resultado += 'Ī';
+						numeroDecimal -= 1000;
+						break;
+				case 1: resultado += 'Ẋ';
+						numeroDecimal -= 10000;
+						break;
+				case 2: resultado += 'Č';
+						numeroDecimal -= 100000;
+						break;
+				case 3: resultado += 'Ḿ';
+						numeroDecimal -= 1000000;
+						break;
+			}
+		}else if(digitos[0] == 4){
+			//insertar switch
+		}else{
+			//caso del 9, insertar switch
+		}
 		return numeroDecimal;
 	}
 
-	public int numeroRomano(int numeroDecimal){
+	public int numeroRomano(int numeroDecimal, int[] digitos){
 		return numeroDecimal;
 	}
 }
